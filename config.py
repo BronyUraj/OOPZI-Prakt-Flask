0fb66ae5a28f9e4c10a3290e64e519cf816810c0
BronyUraj <76591985+BronyUraj@users.noreply.github.com>
2021-02-13 15:22:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    API_KEY = "6797106fd85191ea7335559677948c72" or os.environ.get("API_KEY")
+    API_KEY = "Test" or os.environ.get("API_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
