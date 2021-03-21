c09ffc0a899353c3d603b22db889fa47e7450188
BronyUraj <76591985+BronyUraj@users.noreply.github.com>
2021-03-21 15:03:57 +0300

    Repr removed!

--- app/models.py
+++ app/models.py
@@ -7,6 +7,3 @@ class Records(db.Model):
     YardArea = db.Column(db.Float)
     YardType = db.Column(db.String(64), index=True)
     Coordinates = db.Column(db.String(32))
-
-    def __repr__(self):
-        return (f"Имя мусорки: {self.YardName} Район мусорки: {self.AdmArea} Округ мусорки: {self.District} Адрес мусорки: {self.Address} Площадь мусорки: {self.YardArea} м^2 Тип мусорки: {self.YardType} Координаты мусорки: {self.Coordinates}")
